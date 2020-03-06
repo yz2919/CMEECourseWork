7f84fd2f8edcdae1b6399cf06507e76ef9198892
Yuqing Zhou <yz2919@imperial.ac.uk>
2020-03-06 14:37:58 +0000

    by a bit

---
+++ MiniProject/code/run_MiniProject.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Author: Yuqing Zhou y.zhou@imperial.ac.uk
+# Script: run_MiniProject.sh
+# Desc: run the workflow of miniproject
+# Date: Mar 2020
+
+
+echo -e "\nInstall some necessary packages\n"
+
+python3 Data_preparation.py
+Rscript NLLS_fitting.R
+echo -e "\Running plotting script\n"
+Rscript Plotting_Analysis.R
+mv ../results/Aerobic\ Psychotropic._Raw\ Chicken\ Breast_2_1_2.png ../results/example.png
+
+echo -e "\nGenerating report\n"
+bash CompileLaTeX.sh report 2>/dev/null
+
+## Create a pdf file
+#pdflatex miniproject.tex
+#bibtex miniproject
+##pdflatex miniproject.tex
+#pdflatex miniproject.tex
+#pdflatex miniproject.tex
+
+
+## Cleanup
+#rm *.aux
+#rm *.log
+#rm *.blg
+#rm *.bbl
